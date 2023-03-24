5102b168c13b039b5cf22bf3bfbf4cb23dda51ca
VidarDev <trichier@PC-THEO>
2023-03-24 13:24:19 +0100

    #001 - Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+// import './styles/_main.scss'
+import { OrbitControls, PerspectiveCamera, Environment, Effects } from "@react-three/drei";
+import { Canvas } from '@react-three/fiber';
+import { Suspense, useState } from "react";
+import { Avatar } from "./components/Avatar";
+
+function SceneApp() {
+
+    const [hovered, setHovered] = useState(false);
+    
+    return (
+        <>
+            <Environment files={
+                [
+                    "./assets/environment.webp",
+                    "./assets/environment.webp",
+                    "./assets/environment.webp",
+                    "./assets/environment.webp",
+                    "./assets/environment.webp",
+                    "./assets/environment.webp",
+                ]}
+            />
+            <OrbitControls enableZoom={false} enablePan={false} rotateSpeed={0.01} minAzimuthAngle={-Math.PI / 100} maxAzimuthAngle={Math.PI / 100} minPolarAngle={Math.PI / 2.05} maxPolarAngle={Math.PI - Math.PI / 2.05}/>
+
+            <PerspectiveCamera makeDefault position={[0, 0, 7]} fov={60} zoom={4} />
+            
+            <Avatar position={[0 , -4.25 , 0]} rotation={[-0.25, 0, 0]} />
+            
+            {/* <Icosahedron
+                onPointerEnter={() => setHovered(true)}
+                onPointerLeave={() => setHovered(false)}
+            >
+                <meshBasicMaterial color={hovered ? "yellow" : "blue"} />
+            </Icosahedron> */}
+        
+            <pointLight
+                intensity={0.5}
+                decay={2}
+                color="#9f0003"
+                position={[3.27, 5.9, 8.34]}
+                rotation={[-0.25, 0.38, 1.04]}
+            />
+            <pointLight
+                intensity={1}
+                decay={2}
+                position={[0.04, 7.68, 8.36]}
+                rotation={[-0.25, 0.38, 1.04]}
+            />
+
+            <Effects>
+            </Effects>
+        </>
+    );
+};
+
+function App() {
+    return (
+        <Suspense fallback={null}>
+            <Canvas shadows>
+                <SceneApp />
+            </Canvas>
+        </Suspense>
+    );
+};
+
+export default App;
